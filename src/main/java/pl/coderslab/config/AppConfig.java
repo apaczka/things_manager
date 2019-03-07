7b1c99cdbd8cda47d193cbabb94ba90a608d91d4
Anna Paczkowska-Kańtoch <apacz444@poczta.onet.pl>
2019-03-07 15:04:20 +0100

    adding gitignore and configuration class to project

---
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -0,0 +1,90 @@
+package pl.coderslab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+import java.util.Locale;
+import java.util.Properties;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.coderslab")
+@EnableTransactionManagement
+//@EnableJpaRepositories("pl.coderslab.repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("thingsManagerPersistenceUnit");
+        return emfb; }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm; }
+
+    //
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(getFamilyConverter());
+//        registry.addConverter(getDateConverter());
+//        registry.addConverter(getMemberConverter());
+//    }
+//    @Bean
+//    public FamilyConverter getFamilyConverter () {
+//        return new FamilyConverter();
+//    }
+//    @Bean
+//    public DateConverter getDateConverter(){
+//        return new DateConverter();
+//    }
+//    //
+//    @Bean
+//    public MemberConverter getMemberConverter(){
+//        return new MemberConverter();
+//    }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver; }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+
+
+}
+
